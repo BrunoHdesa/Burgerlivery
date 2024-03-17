6e6423ce5098ceda7024fbba6e0b5de31a4c3a06
BrunoH <desabruno2004@gmail.com>
2024-03-17 00:32:39 -0300

    adicionando o route das paginas

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,21 @@ import Home from "./pages/Home/Home";
 import Hamburgers from "./pages/Hamburgers/Hamburgers";
 import Appetizers from "./pages/Appetizers/Appetizers";
 import Login from "./pages/Login/Login";
+import Combos from "./pages/Combos/Combos";
+import Beverages from "./pages/Beverages/Beverages";
+import Desserts from "./pages/Desserts/Desserts";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/entradinhas" element={<Appetizers />} />
+        <Route path="/entradas" element={<Appetizers />} />
         <Route path="/hamburgers" element={<Hamburgers />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/combos" element={<Combos />} />
+        <Route path="/sobremesas" element={<Desserts />} />
+        <Route path="/bebidas" element={<Beverages />} />
       </Routes>
     </BrowserRouter>
   );
